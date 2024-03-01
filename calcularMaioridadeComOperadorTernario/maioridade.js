dca7a4fe1de84b6fb1b8b4067a42e31730cc7493
Rafaela Souza <130423920+rssrafaela@users.noreply.github.com>
2024-03-01 14:42:32 -0300

    Add files via upload

---
+++ calcularMaioridadeComOperadorTernario/maioridade.js
@@ -0,0 +1,10 @@
+/* Verificação de Maioridade:
+
+Escreva um programa em javaScript que solicite a idade do usuário e verifica se ele é maior de idade (idade igual ou superior a 18 anos). */
+
+let idade = 45;
+let mensagem =
+  idade >= 18
+    ? "O funcionário é maior de idade: " + idade
+    : "O funcionário é menor de idade: " + idade;
+alert(mensagem);
